7042cd6f1eb6310af5da6f71d5a1c4eb8cc8c14f
Ercan-Keskin <ercan.keskin91@gmail.com>
2024-10-29 23:53:44 +0300

    first commit

---
+++ 2-ConsoleLog/consoleLog.js
@@ -0,0 +1,5 @@
+console.log("Ercan");
+console.log("100");
+var a = 5;
+var b = 6;
+console.log(a+b);
---
+++ 3-DegiskenTanimlama/degisken.js
@@ -0,0 +1,24 @@
+var ay= 1;
+var gun=1;
+var yil= 1991;
+
+
+console.log(ay, gun, yil);
+console.log("Bu kişinin doğum tarihi " +ay+"/"+gun+"/"+yil+ " dir");
+console.log("Bu kişinin doğum tarihi", ay,"/",gun,"/",yil, "dir");
+
+
+gun=29;
+console.log(gun);
+
+
+var arabafiyati=180000;
+console.log(arabafiyati);
+
+const sifirarabafiyati = 240000;
+
+console.log(sifirarabafiyati);
+
+//var değiştirilebilir değişkendir.
+//const ise değiştirelemez. Bir kez değer veririz. sonrasında ilk verdiğimiz değişken
+//ile devam eder mesela const gun = 10 dediğimizde ikinci kez const gun 11 diyemeyiz.
